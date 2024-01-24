816746721765eeabed8ee072361d3b9bf1a97442
Farkhunda Saleem <96863402+farkhundaSaleem@users.noreply.github.com>
2024-01-24 18:16:49 +0500

    Update Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -16,7 +16,7 @@ const Signup = () => {
 
   const handleSignup = async () => {
     try {
-      const response = await fetch('your_backend_api/signup', {
+      const response = await fetch('http://localhost:5000/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
